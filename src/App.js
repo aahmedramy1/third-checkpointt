c149ad2c48e8a813d5ac937c4c4565202e6ad426
Ahmed Ramy <ahmedramy.e404@gmail.com>
2021-07-09 19:17:35 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import ElonMusk from './imageInSrc.jpg';
+import "./style.css"
+import './App.css';
+
+// border:solid 1px black;max-width:100vw"
+function App() {
+  return (
+    <div className = "app">
+      <div className ="profile">
+
+        <h1 className ="title red">Ahmed Ramy</h1>
+
+        <br />
+
+        <img src= {ElonMusk} alt = "Elon Musk" />
+
+        <br />
+
+        <img src="/imageInPublic.jpg" alt = "Jeff Bezos" /> 
+
+      </div>
+
+      <video width="320" height="240" controls>
+
+        <source src="myVideo.mp4" type="video/mp4" />
+
+      </video>
+    </div>
+  );
+}
+
+export default App;
